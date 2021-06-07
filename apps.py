588ed9dd003eefcab2f1608bbc29b56ca3270414
MVG-Innovations <83455446+mvg-innovations@users.noreply.github.com>
2021-06-07 10:25:43 +0530

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from bot import *
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    user_text = request.args.get('msg')
+    return str(response(user_text))
+
+
+if __name__ == "__main__":
+    app.run()
